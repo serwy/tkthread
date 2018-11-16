6220f594ad6383a4f69398e0b7c75eed73479f77
Roger D. Serwy <roger.serwy@gmail.com>
2018-11-15 20:36:30 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from setuptools import setup
+from tkthread._version import __version__
+
+with open('README.md', encoding='utf-8') as fid:
+    LONG_DESCRIPTION = fid.read()
 
 setup(
     name='tkthread',
-    version='0.1.0',
+    version=__version__,
     author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
     license="Apache Version 2.0",
     keywords="tkinter threading",
     url="http://github.com/serwy/tkthread",
     packages=['tkthread'],
     description='Multithreading with Tkinter',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: User Interfaces',
+    ],
 
 )
